86f4c9b015c5cbfa76e8029ac99cce7bc3a63b68
Mohammadi El Youzghi <mo.elyouzghi@gmail.com>
2019-12-26 01:01:42 +0100

    Added copyright notice

--- src/Projects/Infrastructure/Doctrine/Repository/DoctrineProjectRepository.php
+++ src/Projects/Infrastructure/Doctrine/Repository/DoctrineProjectRepository.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ *
+ * @copyright 2019 Mohammadi El Youzghi. All rights reserved
+ * @author    Mohammadi El Youzghi (mo.elyouzghi@gmail.com)
+ *
+ * @link      https://github.com/ayrad
+ *
+ * @licence   GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ */
 
 namespace App\Projects\Infrastructure\Doctrine\Repository;
 
