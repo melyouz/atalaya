4f57393d37f102a8e6fa4af98c8e116c1d2cdcf5
Mohammadi El Youzghi <mo.elyouzghi@gmail.com>
2020-01-19 21:31:24 +0100

    bypass finals on tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * @copyright 2020 Mohammadi El Youzghi. All rights reserved
+ * @author    Mohammadi El Youzghi (mo.elyouzghi@gmail.com)
+ *
+ * @link      https://github.com/ayrad
+ *
+ * @licence   GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ */
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../config/bootstrap.php';
+
+DG\BypassFinals::enable();
