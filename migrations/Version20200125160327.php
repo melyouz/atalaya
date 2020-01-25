249ec6b4fb731c6977532c0e1c33bc820b1799e5
Mohammadi El Youzghi <mo.elyouzghi@gmail.com>
2020-01-25 17:06:25 +0100

    Add project createdAt field

---
+++ migrations/Version20200125160327.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20200125160327 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'add project createdAt field';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE project ADD created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE project DROP created_at');
+    }
+}
