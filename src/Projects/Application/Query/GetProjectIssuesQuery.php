df2a9e6462d32b3d5807d806630cd18d6f1fe807
Mohammadi El Youzghi <mo.elyouzghi@gmail.com>
2020-02-15 08:48:33 +0100

    Add endpoint to get project issues

---
+++ src/Projects/Application/Query/GetProjectIssuesQuery.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *
+ * @copyright 2020 Mohammadi El Youzghi. All rights reserved
+ * @author    Mohammadi El Youzghi (mo.elyouzghi@gmail.com)
+ *
+ * @link      https://github.com/ayrad
+ *
+ * @licence   GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ */
+
+declare(strict_types=1);
+
+namespace App\Projects\Application\Query;
+
+use App\Projects\Domain\Model\ProjectId;
+use App\Shared\Application\Query\QueryInterface;
+
+class GetProjectIssuesQuery implements QueryInterface
+{
+    /**
+     * @var ProjectId
+     */
+    private ProjectId $projectId;
+
+    public function __construct(string $projectId)
+    {
+        $this->projectId = ProjectId::fromString($projectId);
+    }
+
+    /**
+     * @return ProjectId
+     */
+    public function getProjectId(): ProjectId
+    {
+        return $this->projectId;
+    }
+}
