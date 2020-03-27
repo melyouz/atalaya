2a0be1066bbee7b8f88c3a1a43c8ded7800b73fa
Mohammadi El Youzghi <mo.elyouzghi@gmail.com>
2020-03-27 22:59:20 +0100

    Add issue createdAt field (#5)

---
+++ migrations/Version20200327215651.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20200327215651 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'add issue createdAt field';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE issue ADD created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE issue DROP created_at');
+    }
+}
