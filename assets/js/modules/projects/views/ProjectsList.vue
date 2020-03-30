e7d5c7cca702c897a09d6688e90385b917eb1bf0
Mohammadi El Youzghi <mo.elyouzghi@gmail.com>
2020-03-30 04:57:35 +0200

    Reformat code

--- assets/js/modules/projects/views/ProjectsList.vue
+++ assets/js/modules/projects/views/ProjectsList.vue
@@ -16,20 +16,23 @@
         </h2>
 
         <v-row v-if="activeProjects.length">
-            <v-col :key="project.id" v-for="project in activeProjects" cols="12" sm="6" lg="4">
+            <v-col :key="project.id" cols="12" lg="4" sm="6" v-for="project in activeProjects">
                 <v-card>
                     <v-card-title>
-                        <v-icon class="mr-1" size="20">{{ projectIcon(project) }}</v-icon> {{ project.name }}
+                        <v-icon class="mr-1" size="20">{{ projectIcon(project) }}</v-icon>
+                        {{ project.name }}
                     </v-card-title>
                     <v-card-subtitle>
-                        <a :href="project.url" class="body-2 external-link" style="text-decoration: none;" target="_blank">
+                        <a :href="project.url" class="body-2 external-link" style="text-decoration: none;"
+                           target="_blank">
                             <v-icon color="secondary" style="text-decoration: none;">mdi-link</v-icon>
                             {{ project.url }}
                         </a>
                     </v-card-subtitle>
                     <v-card-text>
                         <v-layout class="flex justify-start">
-                            <v-text-field :value="projectFullToken(project)" dense filled hide-details label="Project Token"
+                            <v-text-field :value="projectFullToken(project)" dense filled hide-details
+                                          label="Project Token"
                                           readonly/>
                             <v-btn @click="doCopy(projectFullToken(project))" class="mt-3" text>Copy!</v-btn>
                         </v-layout>
@@ -52,27 +55,29 @@
 
         <h2 class="d-block pt-12" v-if="archivedProjects.length">Archived projects</h2>
         <v-row v-if="archivedProjects.length">
-            <v-col :key="project.id" v-for="project in archivedProjects" cols="12" sm="6" lg="4">
-            <v-card>
-                <v-card-title>
-                    <v-icon class="mr-1" size="20">{{ projectIcon(project) }}</v-icon> {{ project.name }}
-                </v-card-title>
-                <v-card-subtitle>
-                    <a :href="project.url" class="body-2 external-link" style="text-decoration: none;" target="_blank">
-                        <v-icon color="secondary" style="text-decoration: none;">mdi-link</v-icon>
-                        {{ project.url }}
-                    </a>
-                </v-card-subtitle>
-                <v-card-actions>
-                    <v-list-item class="grow">
-                        <v-row align="center" justify="end">
-                            <v-btn @click="unarchive(project)" color="primary" text>
-                                <v-icon>mdi-archive-arrow-up</v-icon>
-                            </v-btn>
-                        </v-row>
-                    </v-list-item>
-                </v-card-actions>
-            </v-card>
+            <v-col :key="project.id" cols="12" lg="4" sm="6" v-for="project in archivedProjects">
+                <v-card>
+                    <v-card-title>
+                        <v-icon class="mr-1" size="20">{{ projectIcon(project) }}</v-icon>
+                        {{ project.name }}
+                    </v-card-title>
+                    <v-card-subtitle>
+                        <a :href="project.url" class="body-2 external-link" style="text-decoration: none;"
+                           target="_blank">
+                            <v-icon color="secondary" style="text-decoration: none;">mdi-link</v-icon>
+                            {{ project.url }}
+                        </a>
+                    </v-card-subtitle>
+                    <v-card-actions>
+                        <v-list-item class="grow">
+                            <v-row align="center" justify="end">
+                                <v-btn @click="unarchive(project)" color="primary" text>
+                                    <v-icon>mdi-archive-arrow-up</v-icon>
+                                </v-btn>
+                            </v-row>
+                        </v-list-item>
+                    </v-card-actions>
+                </v-card>
             </v-col>
         </v-row>
 
@@ -82,11 +87,14 @@
                 <v-container>
                     <v-form @keyup.native.enter="create" autocomplete="off" class="pa-2"
                             lazy-validation ref="newProjectForm" v-model="newProjectFormValid">
-                        <v-text-field :rules="[rules.required('Name is required')]" autocapitalize="none" autocorrect="off" placeholder="Name"
+                        <v-text-field :rules="[rules.required('Name is required')]" autocapitalize="none"
+                                      autocorrect="off" placeholder="Name"
                                       required v-model="newProject.name"/>
-                        <v-text-field :rules="[rules.required('URL is required'), rules.url]" autocapitalize="none" autocorrect="off" placeholder="URL"
+                        <v-text-field :rules="[rules.required('URL is required'), rules.url]" autocapitalize="none"
+                                      autocorrect="off" placeholder="URL"
                                       required v-model="newProject.url"/>
-                        <v-select :rules="[rules.required('Platform is required')]" :items="projectAvailablePlatforms()" item-text="name" item-value="name" label="Platform" v-model="newProject.platform" />
+                        <v-select :items="projectAvailablePlatforms()" :rules="[rules.required('Platform is required')]"
+                                  item-text="name" item-value="name" label="Platform" v-model="newProject.platform"/>
                     </v-form>
                 </v-container>
                 <v-card-actions>
@@ -109,7 +117,9 @@
                                       required v-model="editingProject.name"/>
                         <v-text-field :rules="rules.url" autocapitalize="none" autocorrect="off" placeholder="URL"
                                       required v-model="editingProject.url"/>
-                        <v-select :rules="[rules.required('Platform is required')]" :items="projectAvailablePlatforms()" item-text="name" item-value="name" label="Platform" v-model="editingProject.platform" />
+                        <v-select :items="projectAvailablePlatforms()" :rules="[rules.required('Platform is required')]"
+                                  item-text="name" item-value="name" label="Platform"
+                                  v-model="editingProject.platform"/>
                     </v-form>
                 </v-container>
                 <v-card-actions>
