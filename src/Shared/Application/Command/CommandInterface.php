529c4b9aeccbf676c55583258255048ed73fb6dc
Mohammadi El Youzghi <mo.elyouzghi@gmail.com>
2021-05-10 01:52:21 +0200

    Update Copyright

--- src/Shared/Application/Command/CommandInterface.php
+++ src/Shared/Application/Command/CommandInterface.php
@@ -1,10 +1,10 @@
 <?php
-/**
+/*
  *
- * @copyright 2019 Mohammadi El Youzghi. All rights reserved
+ * @copyright 2019-present Mohammadi El Youzghi. All rights reserved
  * @author    Mohammadi El Youzghi (mo.elyouzghi@gmail.com)
  *
- * @link      https://github.com/ayrad
+ * @link      https://github.com/melyouz
  *
  * @licence   GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  *
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,14 @@
 <?php
+/*
+ *
+ * @copyright 2019-present Mohammadi El Youzghi. All rights reserved
+ * @author    Mohammadi El Youzghi (mo.elyouzghi@gmail.com)
+ *
+ * @link      https://github.com/melyouz
+ *
+ * @licence   GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ */
 
 use Symfony\Component\Dotenv\Dotenv;
 
